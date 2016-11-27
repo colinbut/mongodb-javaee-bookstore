c923cae7fc39ffd84d5c6c117cf5d2eef15cb54d
Colin But <colinbut@users.noreply.github.com>
2016-11-27 17:59:25 +0000

    adding controller model methods to buy the book and get list of books from db

---
+++ src/main/java/com/mycompany/mongodb/javaee/bookstore/BookStore.java
@@ -0,0 +1,14 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2016 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package com.mycompany.mongodb.javaee.bookstore;
+
+import javax.enterprise.inject.Model;
+
+@Model
+public class BookStore {
+
+
+}

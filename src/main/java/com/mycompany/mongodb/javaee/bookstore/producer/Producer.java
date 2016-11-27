9ffd2f7ecdcf2e6fd47b91d360fdd7f3c06e7907
Colin But <colinbut@users.noreply.github.com>
2016-11-27 17:59:48 +0000

    added a producer class to produce the mongoclient via CDI

---
+++ src/main/java/com/mycompany/mongodb/javaee/bookstore/producer/Producer.java
@@ -0,0 +1,31 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2016 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package com.mycompany.mongodb.javaee.bookstore.producer;
+
+import com.mongodb.MongoClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import java.net.UnknownHostException;
+
+@ApplicationScoped
+public class Producer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Producer.class);
+
+    @Produces
+    public MongoClient mongoClient() {
+        try {
+            return new MongoClient("localhost", 27017);
+        } catch (UnknownHostException e) {
+            LOGGER.error("{}", e);
+        }
+        return null;
+    }
+
+}

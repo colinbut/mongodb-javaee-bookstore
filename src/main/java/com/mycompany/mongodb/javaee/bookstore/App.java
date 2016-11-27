a5aaae5170078259c27df415f674917e0e2d76b2
Colin But <colinbut@users.noreply.github.com>
2016-11-27 17:02:32 +0000

    generated source code from mvn

---
+++ src/main/java/com/mycompany/mongodb/javaee/bookstore/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.mongodb.javaee.bookstore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
